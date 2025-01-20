8a1acbc00d3ddbcdce007f80de25040d729b3b77
w1f1shower <takanuvalight@gmail.com>
2025-01-21 02:03:50 +0700

    trying to fix some mess 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "app/uploads")
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
     from app.routes import app as routes_blueprint
     app.register_blueprint(routes_blueprint)
